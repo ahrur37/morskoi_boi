385e6186ccee5c77cd67d31581737acc7b2ea21d
Артур Зайнутдинов <ahrur37@gmail.com>
2025-03-11 10:23:40 +0300

    .

--- morskoi_boi/Program.cs
+++ morskoi_boi/Program.cs
@@ -5,188 +5,161 @@ namespace morskoi_boi
     internal class Program
     {
         #region 
-        static char[,] boardBot = new char[10, 10];
-        static char[,] boardHit = new char[10, 10];
-        static char[,] boardIgrok = new char[10, 10];
-        // static char currentPlayer = 'I';
-        // static bool win = false;
+        static char[,] boardBot = new char[10, 10]; 
+        static char[,] boardHit = new char[10, 10]; 
+        static char[,] boardIgrok = new char[10, 10]; 
         static Random rnd = new Random();
-        static int ships1 = 1; // 4
-        static int ships2 = 1; // 3
-        static int ships3 = 1; // 2
-        static int ships4 = 1; // 1
+        static int ships1 = 4; 
+        static int ships2 = 3; 
+        static int ships3 = 2; 
+        static int ships4 = 1; 
         static int x = 0;
         static int y = 0;
         #endregion
+
         static void Main(string[] args)
         {
-            InitializeBoard();
-            PrintGrid();
+            InitializeBoard(); 
+            PrintGrid(); 
         }
-        public static void InitializeBoard() // базовые настройки 
+        public static void InitializeBoard()
         {
             for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    boardBot[i, j] = '~';
-                    boardIgrok[i, j] = '~';
+                    boardBot[i, j] = '~'; 
+                    boardIgrok[i, j] = '~'; 
                 }
             }
             ZapolnenieBot();
-            //ZapolnenieIgrok();
+            ZapolnenieIgrok1();
         }
-        public static void ZapolnenieBot()
+        public static void ZapolnenieBot() // Завершено
         {
-            for (int i = 0; i < 10; i++) // 10 кораблей
+            for (int i = 0; i < ships1; i++)
+                PlaceShip(1);
+            for (int i = 0; i < ships2; i++)
+                PlaceShip(2);
+            for (int i = 0; i < ships3; i++)
+                PlaceShip(3);
+            for (int i = 0; i < ships4; i++)
+                PlaceShip(4);
+        }
+        public static void ZapolnenieIgrok1()
+        {
+            for(int i = 0;i < ships1 + ships2 + ships3 + ships4; i++)
             {
-                x = rnd.Next(1, 9);
-                y = rnd.Next(1, 9);
-                if (boardBot[x, y] == 'S' || boardBot[x, y] == 'N')
-                {
-                    i--;
-                    continue;
-                }
-                boardBot[x, y] = 'S'; // одинарный
-                int sboi = 1;
-                while (sboi == 1)
-                {
-                    if (i > ships1) // одинарный
-                    {
-                        N();
-                        sboi = 0;
-                    }
-                    else if (i >= ships1 && i < ships2) // двойной
-                    {
-                        int v = rnd.Next(0, 3);
-                        if (v == 0 && y - 1 >= 0 && y - 1 <= 9 && boardBot[x, y - 1] != 'N')
-                        {
-                            boardBot[x, y - 1] = 'S';
-                            N();
-                            sboi = 0;
-                        }
-                        else if (v == 1 && y + 1 >= 0 && y + 1 <= 9 && boardBot[x, y + 1] != 'N')
-                        {
-                            boardBot[x, y + 1] = 'S';
-                            N();
-                            sboi = 0;
-                        }
-                        else if (v == 2 && x - 1 >= 0 && x - 1 <= 9 && boardBot[x - 1, y] != 'N')
-                        {
-                            boardBot[x - 1, y] = 'S';
-                            N();
-                            sboi = 0;
-                        }
-                        else if (v == 3 && x + 1 >= 0 && x + 1 <= 9 && boardBot[x + 1, y] != 'N')
-                        {
-                            boardBot[x + 1, y] = 'S';
-                            N();
-                            sboi = 0;
-                        }
-                    }
-                    else if (i >= ships2 && i < ships3) // тройной
-                    {
-                        int v = rnd.Next(0, 3);
-                        if (v == 0 && y - 2 >= 0 && y - 2 <= 9)
-                        {
-                            boardBot[x, y - 1] = 'S';
-                            boardBot[x, y - 2] = 'S';
-                            N();
-                            sboi = 0;
-                        }
-                        else if (v == 1 && y + 2 >= 0 && y + 2 <= 9)
-                        {
-                            boardBot[x, y + 1] = 'S';
-                            boardBot[x, y + 2] = 'S';
-                            N();
-                            sboi = 0;
-                        }
-                        else if (v == 2 && x - 2 >= 0 && x - 2 <= 9)
-                        {
-                            boardBot[x - 1, y] = 'S';
-                            boardBot[x - 2, y] = 'S';
-                            N();
-                            sboi = 0;
-                        }
-                        else if (v == 3 && x + 2 >= 0 && x + 2 <= 9)
-                        {
-                            boardBot[x + 1, y] = 'S';
-                            boardBot[x + 2, y] = 'S';
-                            N();
-                            sboi = 0;
-                        }
-                    }
-                    else if (i >= ships3 && i < ships4) // четверной
-                    {
-                        int v = rnd.Next(0, 3);
-                        if (v == 0 && y - 3 >= 0 && y - 3 <= 9)
-                        {
-                            boardBot[x, y - 1] = 'S';
-                            boardBot[x, y - 2] = 'S';
-                            boardBot[x, y - 3] = 'S';
-                            N();
-                            sboi = 0;
+                if (i < ships1)
+                    ZapolnenieIgrok2(1);
+                else if (i < ships1 + ships2)
+                    ZapolnenieIgrok2(2);
+                else if (i < ships1 + ships2 + ships3)
+                    ZapolnenieIgrok2(3);
+                else if (i < ships1 + ships2 + ships3 + ships4)
+                    ZapolnenieIgrok2(4);
+            }
+        } // Завершено
+        public static void ZapolnenieIgrok2(int size)
+        {
+            Console.WriteLine($"Разместите корабль (размер: {size})");
+            bool placed = false;
 
-                        }
-                        else if (v == 1 && y + 3 >= 0 && y + 3 <= 9)
+            while (!placed)
+            {
+                Console.WriteLine("Текущее состояние вашего поля:");
+                PrintGrid(); 
+
+                Console.Write("Введите координаты: ");
+                string input = Console.ReadLine().ToUpper();
+                if (input.Length < 2) continue;
+
+                int x = input[0] - 'A';
+                int y = int.Parse(input.Substring(1)) - 1;
+
+                Console.Write("Введите направление (H - горизонтально, V - вертикально): ");
+                char direction = Console.ReadLine().ToUpper()[0];
+
+                if (CanPlaceShip(x, y, direction, ))
+                {
+                    PrintGrid();
+                    playerShips.Add(new Ship(size));
+                    placed = true;
+                }
+                else
+                {
+                    Console.WriteLine("Невозможно разместить корабль здесь. Попробуйте снова.");
+                }
+            }
+        }
+        public static void PlaceShip(int length)
+        {
+            bool placed = false;
+            while (!placed)
+            {
+                x = rnd.Next(0, 10);
+                y = rnd.Next(0, 10);
+                int direction = rnd.Next(0, 4); 
+
+                if (CanPlaceShip(direction, length))
+                {
+                    for (int i = 0; i < length; i++)
+                    {
+                        if (direction == 0) boardBot[x - i, y] = 'S'; 
+                        else if (direction == 1) boardBot[x, y + i] = 'S'; 
+                        else if (direction == 2) boardBot[x + i, y] = 'S'; 
+                        else if (direction == 3) boardBot[x, y - i] = 'S'; 
+                    }
+                    placed = true;
+                    N(direction, length); 
+                }
+            }
+        }
+
+        // Проверка возможности размещения корабля
+        public static bool CanPlaceShip(int direction, int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                int newX = x;
+                int newY = y;
+                if (direction == 0) newX = x - i;
+                else if (direction == 1) newY = y + i;
+                else if (direction == 2) newX = x + i;
+                else if (direction == 3) newY = y - i; 
+                if (newX < 0 || newX >= 10 || newY < 0 || newY >= 10 || boardBot[newX, newY] != '~')
+                    return false; 
+            }
+            return true; // Корабль может быть размещен
+        }
+        public static void N(int direction, int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                int newX = x;
+                int newY = y;
+
+                if (direction == 0) newX = x - i; 
+                else if (direction == 1) newY = y + i; 
+                else if (direction == 2) newX = x + i;
+                else if (direction == 3) newY = y - i; 
+
+                for (int dx = -1; dx <= 1; dx++)
+                {
+                    for (int dy = -1; dy <= 1; dy++)
+                    {
+                        int nx = newX + dx;
+                        int ny = newY + dy;
+
+                        if (nx >= 0 && nx < 10 && ny >= 0 && ny < 10 && boardBot[nx, ny] == '~')
                         {
-                            boardBot[x, y + 1] = 'S';
-                            boardBot[x, y + 2] = 'S';
-                            boardBot[x, y + 3] = 'S';
-                            N();
-                            sboi = 0;
-                        }
-                        else if (v == 2 && x - 3 >= 0 && x - 3 <= 9)
-                        {
-                            boardBot[x - 1, y] = 'S';
-                            boardBot[x - 2, y] = 'S';
-                            boardBot[x - 3, y] = 'S';
-                            N();
-                            sboi = 0;
-                        }
-                        else if (v == 3 && x + 3 >= 0 && x + 3 <= 9)
-                        {
-                            boardBot[x + 1, y] = 'S';
-                            boardBot[x + 2, y] = 'S';
-                            boardBot[x + 3, y] = 'S';
-                            N();
-                            sboi = 0;
+                            boardBot[nx, ny] = 'N'; 
                         }
                     }
                 }
             }
         }
-        //public static void ZapolnenieIgrok()
-        //{
-        //    Console.WriteLine($"Введите номер строчки на которую хотите поставить {currentPlayer}");
-        //    int stroc = int.Parse(Console.ReadLine());
-        //    Console.WriteLine($"Введите номер столбеца на который хотите поставить {currentPlayer}");
-        //    int stolb = int.Parse(Console.ReadLine());
-        //    if (boardIgrok[stroc - 1, stolb - 1] == '~')
-        //        boardIgrok[stroc - 1, stolb - 1] = currentPlayer;
-        //    else
-        //        Console.WriteLine("Нельзя поставить туда куда уже поставили");
-        //}
-        public static void N()
-        {
-            if (x - 1 < 10 && x - 1 >= 0 && boardBot[x - 1, y] != 'S')
-                boardBot[x - 1, y] = 'N';
-            if (x + 1 < 10 && x + 1 >= 0 && boardBot[x + 1, y] != 'S')
-                boardBot[x + 1, y] = 'N';
-            if (y - 1 < 10 && y - 1 >= 0 && boardBot[x, y - 1] != 'S')
-                boardBot[x, y - 1] = 'N';
-            if (y + 1 < 10 && y + 1 >= 0 && boardBot[x, y + 1] != 'S')
-                boardBot[x, y + 1] = 'N';
-            if (x - 1 < 10 && x - 1 >= 0 && y + 1 < 10 && y + 1 >= 0 && boardBot[x - 1, y + 1] != 'S')
-                boardBot[x - 1, y + 1] = 'N';
-            if (x - 1 < 10 && x - 1 >= 0 && y - 1 < 10 && y - 1 >= 0 && boardBot[x - 1, y - 1] != 'S')
-                boardBot[x - 1, y - 1] = 'N';
-            if (x + 1 < 10 && x + 1 >= 0 && y - 1 < 10 && y - 1 >= 0 && boardBot[x + 1, y - 1] != 'S')
-                boardBot[x + 1, y - 1] = 'N';
-            if (x + 1 < 10 && x + 1 >= 0 && y + 1 < 10 && y + 1 >= 0 && boardBot[x + 1, y + 1] != 'S')
-                boardBot[x + 1, y + 1] = 'N';
-        }
-        static void PrintGrid() // Вывод поля на экран ЗАКОНЧЕНО
+        static void PrintGrid()
         {
             string probel = "  ";
             Console.WriteLine("   A B C D E F G H I J     A B C D E F G H I J");
@@ -196,11 +169,14 @@ namespace morskoi_boi
                     probel = " ";
                 Console.Write((i + 1) + probel);
                 for (int j = 0; j < 10; j++)
-                    Console.Write(boardIgrok[j, i] + " ");
+                {
+                    Console.Write(boardIgrok[i, j] + " "); // Поле игрока
+                }
                 Console.Write(" " + (i + 1) + probel);
                 for (int j = 0; j < 10; j++)
-                    Console.Write(boardBot[j, i] + " ");
-
+                {
+                    Console.Write(boardBot[i, j] + " "); // Поле бота
+                }
                 Console.WriteLine();
             }
         }
